95d3a55b77ce60f4265335f78a99ddc496602d5b
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2021-12-20 11:48:57 +0100

    updated

--- wp-content/themes/KyaraTheme/index.php
+++ wp-content/themes/KyaraTheme/index.php
@@ -75,6 +75,35 @@
         </div>
     </div>
 
+    <ul>
+    <?php 
+    // Define our WP Query Parameters
+    $the_query = new WP_Query( 'posts_per_page=3' ); ?>
+    
+    
+    <?php 
+    // Start our WP Query
+    while ($the_query -> have_posts()) : $the_query -> the_post(); 
+    // Display the Post Title with Hyperlink
+    ?>
+    
+    
+    <li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+    
+    
+    <li><?php 
+    // Display the Post Excerpt (automatisch text afkorten)
+    the_excerpt(__('(more…)'));
+    ?></li>
+    
+    
+    <?php 
+    // Repeat the process and reset once it hits the limit
+    endwhile;
+    wp_reset_postdata();
+    ?>
+    </ul>
+
     <div class="container block">
         <!-- (row of) Cards -->
         <div class="row">
