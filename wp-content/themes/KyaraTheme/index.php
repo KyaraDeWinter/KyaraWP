512b1874b7548bb9c46b572419c0df2caffffd61
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2021-12-21 13:19:49 +0100

    Update index.php

--- wp-content/themes/KyaraTheme/index.php
+++ wp-content/themes/KyaraTheme/index.php
@@ -116,7 +116,7 @@
 
     <!-- Quote -->
     <div class="quote block">
-        <h4><i class="fas fa-quote-left" style="margin-right: 1rem;"></i>Place 'quote' here<i class="fas fa-quote-right"style="margin-left: 1rem;"></i></h4>
+        <h4><i class="fas fa-quote-left" style="margin-right: 1rem;"></i><?php the_field('quote'); ?><i class="fas fa-quote-right"style="margin-left: 1rem;"></i></h4>
     </div>
 
     <!-- Text/image -->
