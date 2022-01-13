be7f70a9fc7714535e695f155a3d7622ab881297
KyaraDeWinter <70946292+KyaraDeWinter@users.noreply.github.com>
2022-01-13 14:48:11 +0100

    quote

---
+++ wp-content/themes/KyaraTheme/blocks/quote.php
@@ -0,0 +1,4 @@
+<!-- Quote -->
+<div class="quote block">
+    <h4><i class="fas fa-quote-left" style="margin-right: 1rem;"></i><?php the_field('quote'); ?><i class="fas fa-quote-right"style="margin-left: 1rem;"></i></h4>
+</div>
--- wp-content/themes/KyaraTheme/index.php
+++ wp-content/themes/KyaraTheme/index.php
@@ -91,45 +91,13 @@
     <div class="container block">
         <!-- (row of) Cards -->
         <div class="row">
-
-            <?php $the_query = new WP_Query( 'posts_per_page=3' ); // Define our WP Query Parameters ?>
-            
-            <?php 
-            // Start our WP Query
-            while ($the_query -> have_posts()) : $the_query -> the_post(); 
-            // Display the Post Title with Hyperlink
-            ?>
-
-            <div class="col-4">
-                <div class="card">
-
-                <?php 
-                $image = get_field('image');
-                if( !empty( $image ) ): ?>
-                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-                <?php endif; ?>
-
-                <div class="card-body">
-                        <a class="h5" href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-                        <p class="card-text"><?php the_excerpt(__('(more…)')); // Display the Post Excerpt (automatisch text afkorten) ?></p> 
-                    </div>
-                </div>
-            </div>
-
-            <?php 
-            // Repeat the process and reset once it hits the limit
-            endwhile;
-            wp_reset_postdata();
-            ?>
-
+            <?php include 'blog_card.php';?>
         </div>
     </div>
     
 
     <!-- Quote -->
-    <div class="quote block">
-        <h4><i class="fas fa-quote-left" style="margin-right: 1rem;"></i><?php the_field('quote'); ?><i class="fas fa-quote-right"style="margin-left: 1rem;"></i></h4>
-    </div>
+    <?php include 'quote.php';?>
 
     <!-- Text/image -->
     <div class="container textimage block">
